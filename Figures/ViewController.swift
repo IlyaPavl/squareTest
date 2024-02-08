1f8bd06769afe663cd447489f95f996dc3f10b43
Ilya Pavlov <ilyapav@icloud.com>
2024-02-08 15:34:16 +0300

    Initial Commit

---
+++ Figures/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Figures
+//
+//  Created by ily.pavlov on 08.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
